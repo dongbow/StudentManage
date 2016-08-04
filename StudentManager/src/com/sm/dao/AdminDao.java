033b4e0a5e4f09f079a6b3916da6fda603da00a5
wangdongbo <wangdongbo@xiaomadada.com>
2016-08-04 17:55:51 +0800

    all

--- StudentManager/src/com/sm/dao/AdminDao.java
+++
@@ -1,9 +0,0 @@
-package com.sm.dao;
-
-import com.sm.model.Admin;
-
-public interface AdminDao {
-
-	public Admin loginCheck(Admin admin);
-	
-}
--- StudentManager/src/com/sm/service/AdminService.java
+++
@@ -1,9 +0,0 @@
-package com.sm.service;
-
-import com.sm.model.Admin;
-
-public interface AdminService {
-
-	public Admin loginCheck(String loginName, String password);
-	
-}
